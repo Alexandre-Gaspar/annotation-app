9a24620ffe151003e26c82eddcc54b8c8053e30e
Alexandre Gaspar <alexandretiao16@gmail.com>
2024-11-19 12:56:04 +0100

    config(MainActivity): configure navigation routes between screens

--- app/src/main/java/com/alex3g/anotationsapp/MainActivity.kt
+++ app/src/main/java/com/alex3g/anotationsapp/MainActivity.kt
@@ -6,54 +6,36 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.AlertDialogDefaults.containerColor
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.alex3g.anotationsapp.screens.HomeScreen
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.alex3g.anotationsapp.ui.screens.AddNoteScreen
+import com.alex3g.anotationsapp.ui.screens.HomeScreen
+import com.alex3g.anotationsapp.ui.theme.AnotationsAppTheme
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Scaffold(
-                topBar = {
-                    TopAppBar(
-                        colors = topAppBarColors(
-                            containerColor = MaterialTheme.colorScheme.primaryContainer,
-                            titleContentColor = MaterialTheme.colorScheme.primary,
-                        ),
-                        title = {
-                            Text("Top app bar")
-                        }
-                    )
-                },
-                floatingActionButton = {
-                    FloatingActionButton(onClick = { }) {
-                        Icon(Icons.Default.Add, contentDescription = "Add")
-                    }
-                }
-            ) { innerPadding ->
-                Surface(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(innerPadding),
-                    color = MaterialTheme.colorScheme.background,
-                    shadowElevation = 4.dp
-                ) {
+            AnotationsAppTheme {
+                val navController = rememberNavController()
 
+                NavHost(navController = navController, startDestination = "home") {
+                    composable("home") {
+                        HomeScreen(
+                            onAddClick = { navController.navigate("add-note") }
+                        )
+                    }
+                    composable("add-note") {
+                        AddNoteScreen(
+                            onHomeClick = { navController.navigate("home") { popUpTo("home") { inclusive = true } } }
+                        )
+                    }
                 }
             }
         }
